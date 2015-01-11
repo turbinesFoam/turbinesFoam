37b1cd691141603396bcead757c4f6cb668b258e
Pete Bachant <petebachant@gmail.com>
2015-01-10 23:23:31 -0500

    Revert "Add forceField member back to actuatorLineElement"
    This reverts commit be81f240ca38ea3978dcf15a59d157a165974e8f.

--- src/fvOptions/actuatorLineSource/actuatorLineElement/actuatorLineElement.C
+++ src/fvOptions/actuatorLineSource/actuatorLineElement/actuatorLineElement.C
@@ -170,19 +170,7 @@ Foam::fv::actuatorLineElement::actuatorLineElement
 :
     dict_(dict),
     name_(name),
-    mesh_(mesh),
-    forceField_
-    (
-        IOobject
-        (
-            "actuatorLineElementForce",
-            mesh_,
-            IOobject::NO_READ,
-            IOobject::NO_WRITE
-        ),
-        mesh_,
-        dimensionedVector("force", dimForce/dimVolume/dimDensity, vector::zero)
-    )
+    mesh_(mesh)
 {
     velocity_ = vector::zero;
     forceVector_ = vector::zero;
