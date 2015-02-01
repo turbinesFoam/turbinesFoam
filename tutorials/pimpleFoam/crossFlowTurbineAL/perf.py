5a17876a56f366ebeb1d2fdbb5558b69ec95ce42
Pete Bachant <petebachant@gmail.com>
2015-02-01 10:51:14 -0500

    Modify perf plotting script to take into account more quantities and initial angle

--- tutorials/pimpleFoam/crossFlowTurbineAL/perf.py
+++ tutorials/pimpleFoam/crossFlowTurbineAL/perf.py
@@ -11,11 +11,16 @@ import os
 plt.style.use("ggplot")
 savefig = False
 
-def plot_cp():
+def plot_cp(angle0=540.0):
     df = pd.read_csv("postProcessing/turbines/0/turbine.csv")
-    print("Mean C_P =", df.cp.mean())
-    plt.plot(df.time, df.cp)
-    plt.xlabel("Time (s)")
+    if df.angle_deg.max() < angle0:
+        angle0 = 0.0
+    print("Performance from {} degrees onward".format(angle0))
+    print("Mean TSR =", df.tsr[df.angle_deg >= angle0].mean())
+    print("Mean C_P =", df.cp[df.angle_deg >= angle0].mean())
+    print("Mean C_D =", df.cd[df.angle_deg >= angle0].mean())
+    plt.plot(df.angle_deg, df.cp)
+    plt.xlabel("Azimuthal angle (degrees)")
     plt.ylabel("$C_P$")
     if savefig:
         if not os.path.isdir("figures"):
