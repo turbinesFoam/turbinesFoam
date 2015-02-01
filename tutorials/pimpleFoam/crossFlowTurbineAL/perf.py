0adf71a9a7024a078220166c9473bc5d191a2124
Pete Bachant <petebachant@gmail.com>
2015-02-01 12:24:50 -0500

    Minor change to perf.py in CFT AL tutorial

--- tutorials/pimpleFoam/crossFlowTurbineAL/perf.py
+++ tutorials/pimpleFoam/crossFlowTurbineAL/perf.py
@@ -15,7 +15,7 @@ def plot_cp(angle0=540.0):
     df = pd.read_csv("postProcessing/turbines/0/turbine.csv")
     if df.angle_deg.max() < angle0:
         angle0 = 0.0
-    print("Performance from {} degrees onward".format(angle0))
+    print("Performance from {} degrees onward:".format(angle0))
     print("Mean TSR =", df.tsr[df.angle_deg >= angle0].mean())
     print("Mean C_P =", df.cp[df.angle_deg >= angle0].mean())
     print("Mean C_D =", df.cd[df.angle_deg >= angle0].mean())
