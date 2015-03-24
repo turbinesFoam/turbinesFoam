5a7bb5e2d878850e792f4ca5aac1a16a84e3fe23
Pete Bachant <petebachant@gmail.com>
2015-03-24 10:29:27 -0400

    Print perf to 2 decimals

--- tutorials/pimpleFoam/crossFlowTurbineAL/perf.py
+++ tutorials/pimpleFoam/crossFlowTurbineAL/perf.py
@@ -16,9 +16,9 @@ def plot_cp(angle0=540.0):
     if df.angle_deg.max() < angle0:
         angle0 = 0.0
     print("Performance from {} degrees onward:".format(angle0))
-    print("Mean TSR =", df.tsr[df.angle_deg >= angle0].mean())
-    print("Mean C_P =", df.cp[df.angle_deg >= angle0].mean())
-    print("Mean C_D =", df.cd[df.angle_deg >= angle0].mean())
+    print("Mean TSR = {:.2f}".format(df.tsr[df.angle_deg >= angle0].mean()))
+    print("Mean C_P = {:.2f}".format(df.cp[df.angle_deg >= angle0].mean()))
+    print("Mean C_D = {:.2f}".format(df.cd[df.angle_deg >= angle0].mean()))
     plt.plot(df.angle_deg, df.cp)
     plt.xlabel("Azimuthal angle (degrees)")
     plt.ylabel("$C_P$")
