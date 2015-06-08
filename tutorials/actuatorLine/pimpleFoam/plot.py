c24522e13766cff95180eb4505c0adad7630dd98
Pete Bachant <petebachant@gmail.com>
2015-06-08 15:02:13 -0400

    Clean up unsteady AL tutorial

---
+++ tutorials/actuatorLine/pimpleFoam/plot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Visualization for OpenFOAM actuatorLine simulation.
+"""
+from __future__ import division, print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def loadperf():
+    df = pd.read_csv("postProcessing/actuatorLines/0/foil.csv")
+    df = df.drop_duplicates("time", take_last=True)
+    df["alpha_rad"] = df.alpha_deg/180.0*np.pi
+    df["cn"] =  df.cl*np.cos(df.alpha_rad) - df.cd*np.sin(df.alpha_rad)
+    df["ct"] = df.cl*np.sin(df.alpha_rad) - df.cd*np.cos(df.alpha_rad)
+    return df
+    
+def plot_alpha():
+    df = loadperf()
+    plt.figure()
+    plt.plot(df.time, df.alpha_geom_deg, label="Geometric")
+    plt.plot(df.time, df.alpha_deg, label="Actual")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Angle of attack (deg)")
+    plt.legend(loc="best")
+    plt.show()
+    
+def plot_cn(t0=1.0):
+    df = loadperf()
+    plt.figure()
+    ind = df.time >= t0
+    plt.plot(df.alpha_geom_deg[ind], df.cn[ind], "-o")
+    plt.xlabel(r"$\alpha$ (geometric, degrees)")
+    plt.ylabel(r"$C_N$")
+    plt.show()
+
+if __name__ == "__main__":
+    plot_alpha()
+    plot_cn()
