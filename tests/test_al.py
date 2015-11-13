f74a5211255b3d7610601dc6ab9513a2187a955c
Pete Bachant <petebachant@gmail.com>
2015-11-12 22:59:37 -0500

    Simplify AL tests

--- tests/test_al.py
+++ tests/test_al.py
@@ -15,8 +15,6 @@ output_fpath = "postProcessing/actuatorLines/0/foil.csv"
 
 def setup():
     os.chdir("actuatorLineSource")
-    output_clean = subprocess.check_output("./Allclean")
-    output_run = subprocess.check_output(["./Allrun", str(alpha_deg)])
 
 
 def load_output():
@@ -24,18 +22,18 @@ def load_output():
     return pd.read_csv(output_fpath)
 
 
-def test_created():
+def check_created():
     """Test that actuatorLineSource was created."""
     txt = "Selecting finite volume options model type actuatorLineSource"
     subprocess.check_output(["grep", txt, "log.simpleFoam"])
 
 
-def test_output_file_exists():
+def check_output_file_exists():
     """Test that actuatorLineSource output file exists."""
     assert os.path.isfile(output_fpath)
 
 
-def test_geometric_alpha():
+def check_geometric_alpha():
     """
     Test that actuatorLineSource geometric angle of attack was set properly.
     """
@@ -43,9 +41,18 @@ def test_geometric_alpha():
     assert np.all(df.alpha_geom_deg == alpha_deg)
 
 
+def test_2d():
+    """Test 2-D actuatorLineSource."""
+    output_clean = subprocess.check_output("./Allclean")
+    output_run = subprocess.check_output(["./Allrun", str(alpha_deg)])
+    check_created()
+    check_output_file_exists()
+    check_geometric_alpha()
+
+
 def test_alpha_sweep():
-    """Test 2-D actuator line angle of attack sweep."""
-    out = subprocess.check_output(["python", "paramsweep.py", "-15", "16", "5"])
+    """Test 2-D actuatorLineSource angle of attack sweep."""
+    out = subprocess.check_output(["python", "paramsweep.py", "-10", "11", "5"])
     df = pd.read_csv("processed/alpha_sweep.csv")
     mse = np.mean((df.alpha_geom_deg - df.alpha_deg)**2)
     print("Mean square error between geometric and detected alpha (deg):", mse)
@@ -53,14 +60,14 @@ def test_alpha_sweep():
 
 
 def test_3d():
-    """Test 3-D actuator line."""
+    """Test 3-D actuatorLineSource."""
     out = subprocess.check_output("./Allclean")
     out = subprocess.check_output(["./Allrun3D", str(alpha_deg)])
 
 
 @timed(30) # Test must run faster than 30 seconds
 def test_parallel():
-    """Test 3-D actuator line in parallel."""
+    """Test 3-D actuatorLineSource in parallel."""
     out = subprocess.check_output("./Allclean")
     out = subprocess.check_output(["./Allrun3D", "-parallel", str(alpha_deg)])
 
