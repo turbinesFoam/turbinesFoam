612a4e6f8d56af7a92e069f7c3a1044e79685fab
Pete Bachant <petebachant@gmail.com>
2016-01-14 00:43:35 -0500

    Remove turbulence model since fvOptions are now in OF-dev

--- tests/test_libs.py
+++ tests/test_libs.py
@@ -7,8 +7,3 @@ import ctypes
 def test_turbinesfoam():
     """Test that `libturbinesFoam.so` can be loaded."""
     ctypes.cdll.LoadLibrary("libturbinesFoam.so")
-
-
-def test_turbulence_models():
-    """Test that `libturbinesFoamTurbulenceModels.so` can be loaded."""
-    ctypes.cdll.LoadLibrary("libturbinesFoamTurbulenceModels.so")
