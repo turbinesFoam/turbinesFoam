4e079e9a317dddc367245b61c819d339f6cc0b79
Pete Bachant <petebachant@gmail.com>
2016-04-13 09:32:47 -0400

    Fix profile3D script; resolves #214

--- tests/actuatorLineSource/profile3D.sh
+++ tests/actuatorLineSource/profile3D.sh
@@ -4,11 +4,10 @@ cd ${0%/*} || exit 1    # run from this directory
 # Source tutorial run functions
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
 
-TUTORIAL_DIR="../../tutorials/actuatorLine/static"
-
 # Copy files from tutorial
-cp -rf $TUTORIAL_DIR/0.org 0
-cp $TUTORIAL_DIR/system/blockMeshDict system/blockMeshDict-3D
+./getTutorialFiles.sh static
+
+cp -rf 0.org 0
 
 runApplication blockMesh -dict system/blockMeshDict-3D
 runApplication snappyHexMesh -overwrite
@@ -18,10 +17,15 @@ runApplication topoSet
 if [ "$1" = "-parallel" ]
     then
     python scripts/set_alpha.py $2 -d
-    nProc=$(getNumberOfProcessors)
+    if [ $WM_PROJECT_VERSION = "dev" ]
+        then
+        nProc=""
+    else
+        nProc=$(getNumberOfProcessors)
+    fi
     runApplication decomposePar
     ls -d processor* | xargs -I {} rm -rf ./{}/0
-    ls -d processor* | xargs -I {} cp -r $TUTORIAL_DIR/0.org ./{}/0
+    ls -d processor* | xargs -I {} cp -r 0.org ./{}/0
     mpirun -np $nProc valgrind --tool=callgrind simpleFoam -parallel > log.simpleFoam
     runApplication reconstructPar
 else
