c8ef4b53b2649c47c1b884aec13320a32f5a979b
Pete Bachant <petebachant@gmail.com>
2017-07-13 19:51:10 -0400

    Add deps for of+

--- bin/install-of-ubuntu.sh
+++ bin/install-of-ubuntu.sh
@@ -7,6 +7,7 @@ if [ "$OF_VERS" = "1706" ]; then
     wget https://sourceforge.net/projects/openfoamplus/files/v1706/OpenFOAM-v1706-windows10.tgz
     sudo tar -xvzf  OpenFOAM-v1706-windows10.tgz -C /opt/
     sudo mv /opt/OpenFOAM/OpenFOAM-v1706 /opt/openfoam1706
+    sudo apt-get install -qq bison flex m4
 else
     sudo add-apt-repository http://dl.openfoam.org/ubuntu
     sudo sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
