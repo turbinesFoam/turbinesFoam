83f9b615f5a0682284affd4b29ca2e9592114a30
Pete Bachant <petebachant@gmail.com>
2017-07-13 19:42:29 -0400

    Add script for installing on Ubuntu and add 1706 to version list

---
+++ bin/install-of-ubuntu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Install OpenFOAM for Ubuntu--handy for CI
+
+OF_VERS="$1"
+
+if [ "$OF_VERS" = "1706" ]; then
+    wget https://sourceforge.net/projects/openfoamplus/files/v1706/OpenFOAM-v1706-windows10.tgz
+    sudo tar -xvzf  OpenFOAM-v1706-Windows10.tgz -C /opt/
+    sudo mv /opt/OpenFOAM/OpenFOAM-v1706 /opt/openfoam1706
+else
+    sudo add-apt-repository http://dl.openfoam.org/ubuntu
+    sudo sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
+    sudo apt-get update -qq
+    sudo apt-get install -qq openfoam${OF_VERS}
+fi
