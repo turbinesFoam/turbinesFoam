07134991bebbe47ed08091cd9b09e2102c83af17
Anders Goude <anders.goude@gmail.com>
2017-07-17 13:03:43 +0200

    Corrected expression for the corrected radius with respect to the attachment point

--- src/fvOptions/crossFlowTurbineALSource/crossFlowTurbineALSource.C
+++ src/fvOptions/crossFlowTurbineALSource/crossFlowTurbineALSource.C
@@ -140,7 +140,7 @@ void Foam::fv::crossFlowTurbineALSource::createBlades()
             // Move along radial direction
             point += radius*radialDirection_;
             // Set initial velocity of quarter chord
-            scalar radiusCorr = sqrt(magSqr(chordMount - 0.25)*chordLength
+            scalar radiusCorr = sqrt(magSqr((chordMount - 0.25)*chordLength)
                                      + magSqr(radius));
             vector initialVelocity = -freeStreamDirection_*omega_*radiusCorr;
             scalar velAngle = atan2(((chordMount - 0.25)*chordLength), radius);
