06fea86f0feb3068d627195c7eacd3a41218d797
Pete Bachant <petebachant@gmail.com>
2023-11-23 17:56:14 -0800

    Blacken paramsweep.py

--- tutorials/actuatorLine/static/paramsweep.py
+++ tutorials/actuatorLine/static/paramsweep.py
@@ -12,14 +12,28 @@ def read_force_coeffs():
     """Read force coefficients from `postProcessing/actuatorLines`."""
     df = pd.read_csv("postProcessing/actuatorLines/0/foil.csv")
     df = df.iloc[-1]
-    return df[["time", "rel_vel_mag", "alpha_geom_deg", "alpha_deg", "cl",
-               "cd", "cm"]]
+    return df[
+        [
+            "time",
+            "rel_vel_mag",
+            "alpha_geom_deg",
+            "alpha_deg",
+            "cl",
+            "cd",
+            "cm",
+        ]
+    ]
 
 
 def read_turbulence_fields():
     """Dummy function for now."""
-    return {"k": np.nan, "omega": np.nan, "epsilon": np.nan, "nut": np.nan,
-            "z_turbulence": np.nan}
+    return {
+        "k": np.nan,
+        "omega": np.nan,
+        "epsilon": np.nan,
+        "nut": np.nan,
+        "z_turbulence": np.nan,
+    }
 
 
 def alpha_sweep(start, stop, step, append=False):
@@ -29,9 +43,22 @@ def alpha_sweep(start, stop, step, append=False):
     if append:
         df = pd.read_csv(df_fname)
     else:
-        df = pd.DataFrame(columns=["time", "rel_vel_mag", "alpha_geom_deg",
-                                   "alpha_deg", "cl", "cd", "cm", "k", "omega",
-                                   "epsilon", "nut", "z_turbulence"])
+        df = pd.DataFrame(
+            columns=[
+                "time",
+                "rel_vel_mag",
+                "alpha_geom_deg",
+                "alpha_deg",
+                "cl",
+                "cd",
+                "cm",
+                "k",
+                "omega",
+                "epsilon",
+                "nut",
+                "z_turbulence",
+            ]
+        )
     for alpha in alpha_list:
         call("./Allclean")
         call(["./Allrun", "2D", str(alpha)])
@@ -45,16 +72,39 @@ if __name__ == "__main__":
     if not os.path.isdir("processed"):
         os.mkdir("processed")
 
-    parser = argparse.ArgumentParser(description="Vary the foil angle of \
-                                     attack and log results.")
-    parser.add_argument("start", type=float, help="Start angle of sweep.",
-                        nargs="?", default=-15.0)
-    parser.add_argument("stop", type=float, help="End angle of sweep. The sweep\
-                        does not include this value.", nargs="?", default=15.0)
-    parser.add_argument("step", type=float, default=1.0, nargs="?",
-                        help="Spacing between values.")
-    parser.add_argument("--append", "-a", action="store_true", default=False,
-                        help="Append to previous results")
+    parser = argparse.ArgumentParser(
+        description="Vary the foil angle of \
+                                     attack and log results."
+    )
+    parser.add_argument(
+        "start",
+        type=float,
+        help="Start angle of sweep.",
+        nargs="?",
+        default=-15.0,
+    )
+    parser.add_argument(
+        "stop",
+        type=float,
+        help="End angle of sweep. The sweep\
+                        does not include this value.",
+        nargs="?",
+        default=15.0,
+    )
+    parser.add_argument(
+        "step",
+        type=float,
+        default=1.0,
+        nargs="?",
+        help="Spacing between values.",
+    )
+    parser.add_argument(
+        "--append",
+        "-a",
+        action="store_true",
+        default=False,
+        help="Append to previous results",
+    )
     args = parser.parse_args()
 
     alpha_sweep(args.start, args.stop, args.step, append=args.append)
