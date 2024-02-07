0a8160c505e82d2eac612521baea588da32f3aa7
Pete Bachant <petebachant@gmail.com>
2024-02-07 05:29:41 -0800

    Copy over and rename actuationDiskSource as unsteady

---
+++ src/fvOptions/unsteadyActuatorDiskSource/unsteadyActuatorDiskSource.C
@@ -0,0 +1,315 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2016 OpenFOAM Foundation
+    Copyright (C) 2020 ENERCON GmbH
+    Copyright (C) 2018-2022 OpenCFD Ltd
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "unsteadyActuatorDiskSource.H"
+#include "geometricOneField.H"
+#include "cellSet.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace fv
+{
+    defineTypeNameAndDebug(unsteadyActuatorDiskSource, 0);
+    addToRunTimeSelectionTable(option, unsteadyActuatorDiskSource, dictionary);
+}
+}
+
+
+const Foam::Enum
+<
+    Foam::fv::unsteadyActuatorDiskSource::forceMethodType
+>
+Foam::fv::unsteadyActuatorDiskSource::forceMethodTypeNames
+({
+    { forceMethodType::FROUDE, "Froude" },
+    { forceMethodType::VARIABLE_SCALING, "variableScaling" },
+});
+
+
+const Foam::Enum
+<
+    Foam::fv::unsteadyActuatorDiskSource::monitorMethodType
+>
+Foam::fv::unsteadyActuatorDiskSource::monitorMethodTypeNames
+({
+    { monitorMethodType::POINTS, "points" },
+    { monitorMethodType::CELLSET, "cellSet" },
+});
+
+
+// * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
+
+void Foam::fv::unsteadyActuatorDiskSource::writeFileHeader(Ostream& os)
+{
+    writeFile::writeHeader(os, "Actuation disk source");
+    writeFile::writeCommented(os, "Time");
+    writeFile::writeCommented(os, "Uref");
+    writeFile::writeCommented(os, "Cp");
+    writeFile::writeCommented(os, "Ct");
+
+    if (forceMethod_ == forceMethodType::FROUDE)
+    {
+        writeFile::writeCommented(os, "a");
+        writeFile::writeCommented(os, "T");
+    }
+    else if (forceMethod_ == forceMethodType::VARIABLE_SCALING)
+    {
+        writeFile::writeCommented(os, "Udisk");
+        writeFile::writeCommented(os, "CpStar");
+        writeFile::writeCommented(os, "CtStar");
+        writeFile::writeCommented(os, "T");
+        writeFile::writeCommented(os, "P");
+    }
+
+    os  << endl;
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::fv::unsteadyActuatorDiskSource::setMonitorCells(const dictionary& dict)
+{
+    switch (monitorMethod_)
+    {
+        case monitorMethodType::POINTS:
+        {
+            Info<< "    - selecting cells using points" << endl;
+
+            labelHashSet selectedCells;
+
+            List<point> monitorPoints;
+
+            const dictionary* coeffsDictPtr = dict.findDict("monitorCoeffs");
+            if (coeffsDictPtr)
+            {
+                coeffsDictPtr->readIfPresent("points", monitorPoints);
+            }
+            else
+            {
+                monitorPoints.resize(1);
+                dict.readEntry("upstreamPoint", monitorPoints.first());
+            }
+
+            for (const point& p : monitorPoints)
+            {
+                const label celli = mesh_.findCell(p);
+
+                const bool found = (celli >= 0);
+
+                if (found)
+                {
+                    selectedCells.insert(celli);
+                }
+
+                if (!returnReduceOr(found))
+                {
+                    WarningInFunction
+                        << "No owner cell found for point "
+                        << p << endl;
+                }
+            }
+
+            monitorCells_ = selectedCells.sortedToc();
+            break;
+        }
+        case monitorMethodType::CELLSET:
+        {
+            Info<< "    - selecting cells using cellSet "
+                << zoneName() << endl;
+
+            monitorCells_ = cellSet(mesh_, zoneName()).sortedToc();
+            break;
+        }
+        default:
+        {
+            FatalErrorInFunction
+                << "Unknown type for monitoring of incoming velocity"
+                << monitorMethodTypeNames[monitorMethod_]
+                << ". Valid monitor method types : "
+                << monitorMethodTypeNames
+                << exit(FatalError);
+        }
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::fv::unsteadyActuatorDiskSource::unsteadyActuatorDiskSource
+(
+    const word& name,
+    const word& modelType,
+    const dictionary& dict,
+    const fvMesh& mesh
+)
+:
+    fv::cellSetOption(name, modelType, dict, mesh),
+    writeFile(mesh, name, modelType, coeffs_),
+    forceMethod_
+    (
+        forceMethodTypeNames.getOrDefault
+        (
+            "variant",
+            coeffs_,
+            forceMethodType::FROUDE
+        )
+    ),
+    monitorMethod_
+    (
+        monitorMethodTypeNames.getOrDefault
+        (
+            "monitorMethod",
+            coeffs_,
+            monitorMethodType::POINTS
+        )
+    ),
+    sink_
+    (
+        coeffs_.getOrDefault<bool>("sink", true)
+      ? 1
+      : -1
+    ),
+    writeFileStart_(coeffs_.getOrDefault<scalar>("writeFileStart", 0)),
+    writeFileEnd_(coeffs_.getOrDefault<scalar>("writeFileEnd", VGREAT)),
+    diskArea_
+    (
+        coeffs_.getCheck<scalar>
+        (
+            "diskArea",
+            scalarMinMax::ge(VSMALL)
+        )
+    ),
+    diskDir_
+    (
+        coeffs_.getCheck<vector>
+        (
+            "diskDir",
+            [&](const vector& vec){ return mag(vec) > VSMALL; }
+        ).normalise()
+    ),
+    UvsCpPtr_(Function1<scalar>::New("Cp", coeffs_, &mesh)),
+    UvsCtPtr_(Function1<scalar>::New("Ct", coeffs_, &mesh)),
+    monitorCells_()
+{
+    setMonitorCells(coeffs_);
+
+    fieldNames_.resize(1, "U");
+
+    fv::option::resetApplied();
+
+    Info<< "    - creating actuation disk zone: " << this->name() << endl;
+
+    Info<< "    - force computation method: "
+        << forceMethodTypeNames[forceMethod_] << endl;
+
+    writeFileHeader(file());
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::fv::unsteadyActuatorDiskSource::addSup
+(
+    fvMatrix<vector>& eqn,
+    const label fieldi
+)
+{
+    if (V() > VSMALL)
+    {
+        calc(geometricOneField(), geometricOneField(), eqn);
+    }
+}
+
+
+void Foam::fv::unsteadyActuatorDiskSource::addSup
+(
+    const volScalarField& rho,
+    fvMatrix<vector>& eqn,
+    const label fieldi
+)
+{
+    if (V() > VSMALL)
+    {
+        calc(geometricOneField(), rho, eqn);
+    }
+}
+
+
+void Foam::fv::unsteadyActuatorDiskSource::addSup
+(
+    const volScalarField& alpha,
+    const volScalarField& rho,
+    fvMatrix<vector>& eqn,
+    const label fieldi
+)
+{
+    if (V() > VSMALL)
+    {
+        calc(alpha, rho, eqn);
+    }
+}
+
+
+bool Foam::fv::unsteadyActuatorDiskSource::read(const dictionary& dict)
+{
+    if (fv::cellSetOption::read(dict) && writeFile::read(dict))
+    {
+        dict.readIfPresent("sink", sink_);
+        dict.readIfPresent("writeFileStart", writeFileStart_);
+        dict.readIfPresent("writeFileEnd", writeFileEnd_);
+        dict.readIfPresent("diskArea", diskArea_);
+        if (diskArea_ < VSMALL)
+        {
+            FatalIOErrorInFunction(dict)
+                << "Actuator disk has zero area: "
+                << "diskArea = " << diskArea_
+                << exit(FatalIOError);
+        }
+
+        dict.readIfPresent("diskDir", diskDir_);
+        diskDir_.normalise();
+        if (mag(diskDir_) < VSMALL)
+        {
+            FatalIOErrorInFunction(dict)
+                << "Actuator disk surface-normal vector is zero: "
+                << "diskDir = " << diskDir_
+                << exit(FatalIOError);
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+
+// ************************************************************************* //
---
+++ src/fvOptions/unsteadyActuatorDiskSource/unsteadyActuatorDiskSourceTemplates.C
@@ -0,0 +1,248 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2016 OpenFOAM Foundation
+    Copyright (C) 2020 ENERCON GmbH
+    Copyright (C) 2018-2020 OpenCFD Ltd
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "unsteadyActuatorDiskSource.H"
+#include "fvMesh.H"
+#include "fvMatrix.H"
+#include "volFields.H"
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+template<class AlphaFieldType, class RhoFieldType>
+void Foam::fv::unsteadyActuatorDiskSource::calc
+(
+    const AlphaFieldType& alpha,
+    const RhoFieldType& rho,
+    fvMatrix<vector>& eqn
+)
+{
+    switch (forceMethod_)
+    {
+        case forceMethodType::FROUDE:
+        {
+            calcFroudeMethod(alpha, rho, eqn);
+            break;
+        }
+
+        case forceMethodType::VARIABLE_SCALING:
+        {
+            calcVariableScalingMethod(alpha, rho, eqn);
+            break;
+        }
+
+        default:
+            break;
+    }
+}
+
+
+template<class AlphaFieldType, class RhoFieldType>
+void Foam::fv::unsteadyActuatorDiskSource::calcFroudeMethod
+(
+    const AlphaFieldType& alpha,
+    const RhoFieldType& rho,
+    fvMatrix<vector>& eqn
+)
+{
+    const vectorField& U = eqn.psi();
+    vectorField& Usource = eqn.source();
+    const scalarField& cellsV = mesh_.V();
+
+    // Compute upstream U and rho, spatial-averaged over monitor-region
+    vector Uref(Zero);
+    scalar rhoRef = 0.0;
+    label szMonitorCells = monitorCells_.size();
+
+    for (const label celli : monitorCells_)
+    {
+        Uref += U[celli];
+        rhoRef = rhoRef + rho[celli];
+    }
+    reduce(Uref, sumOp<vector>());
+    reduce(rhoRef, sumOp<scalar>());
+    reduce(szMonitorCells, sumOp<label>());
+
+    if (szMonitorCells == 0)
+    {
+        FatalErrorInFunction
+            << "No cell is available for incoming velocity monitoring."
+            << exit(FatalError);
+    }
+
+    Uref /= szMonitorCells;
+    rhoRef /= szMonitorCells;
+
+    const scalar Ct = sink_*UvsCtPtr_->value(mag(Uref));
+    const scalar Cp = sink_*UvsCpPtr_->value(mag(Uref));
+
+    if (Cp <= VSMALL || Ct <= VSMALL)
+    {
+        FatalErrorInFunction
+           << "Cp and Ct must be greater than zero." << nl
+           << "Cp = " << Cp << ", Ct = " << Ct
+           << exit(FatalError);
+    }
+
+    // (BJSB:Eq. 3.9)
+    const scalar a = 1.0 - Cp/Ct;
+    const scalar T = 2.0*rhoRef*diskArea_*magSqr(Uref & diskDir_)*a*(1 - a);
+
+    for (const label celli : cells_)
+    {
+        Usource[celli] += ((cellsV[celli]/V())*T)*diskDir_;
+    }
+
+    if
+    (
+        mesh_.time().timeOutputValue() >= writeFileStart_
+     && mesh_.time().timeOutputValue() <= writeFileEnd_
+    )
+    {
+        Ostream& os = file();
+        writeCurrentTime(os);
+
+        os  << Uref << tab << Cp << tab << Ct << tab << a << tab << T
+            << endl;
+    }
+}
+
+
+template<class AlphaFieldType, class RhoFieldType>
+void Foam::fv::unsteadyActuatorDiskSource::calcVariableScalingMethod
+(
+    const AlphaFieldType& alpha,
+    const RhoFieldType& rho,
+    fvMatrix<vector>& eqn
+)
+{
+    const vectorField& U = eqn.psi();
+    vectorField& Usource = eqn.source();
+    const scalarField& cellsV = mesh_.V();
+
+    // Monitor and average monitor-region U and rho
+    vector Uref(Zero);
+    scalar rhoRef = 0.0;
+    label szMonitorCells = monitorCells_.size();
+
+    for (const label celli : monitorCells_)
+    {
+        Uref += U[celli];
+        rhoRef = rhoRef + rho[celli];
+    }
+    reduce(Uref, sumOp<vector>());
+    reduce(rhoRef, sumOp<scalar>());
+    reduce(szMonitorCells, sumOp<label>());
+
+    if (szMonitorCells == 0)
+    {
+        FatalErrorInFunction
+            << "No cell is available for incoming velocity monitoring."
+            << exit(FatalError);
+    }
+
+    Uref /= szMonitorCells;
+    const scalar magUref = mag(Uref);
+    rhoRef /= szMonitorCells;
+
+    // Monitor and average U and rho on actuator disk
+    vector Udisk(Zero);
+    scalar rhoDisk = 0.0;
+    scalar totalV = 0.0;
+
+    for (const label celli : cells_)
+    {
+        Udisk += U[celli]*cellsV[celli];
+        rhoDisk += rho[celli]*cellsV[celli];
+        totalV += cellsV[celli];
+    }
+    reduce(Udisk, sumOp<vector>());
+    reduce(rhoDisk, sumOp<scalar>());
+    reduce(totalV, sumOp<scalar>());
+
+    if (totalV < SMALL)
+    {
+        FatalErrorInFunction
+            << "No cell in the actuator disk."
+            << exit(FatalError);
+    }
+
+    Udisk /= totalV;
+    const scalar magUdisk = mag(Udisk);
+    rhoDisk /= totalV;
+
+    if (mag(Udisk) < SMALL)
+    {
+        FatalErrorInFunction
+            << "Velocity spatial-averaged on actuator disk is zero." << nl
+            << "Please check if the initial U field is zero."
+            << exit(FatalError);
+    }
+
+    // Interpolated thrust/power coeffs from power/thrust curves
+    const scalar Ct = sink_*UvsCtPtr_->value(magUref);
+    const scalar Cp = sink_*UvsCpPtr_->value(magUref);
+
+    if (Cp <= VSMALL || Ct <= VSMALL)
+    {
+        FatalErrorInFunction
+           << "Cp and Ct must be greater than zero." << nl
+           << "Cp = " << Cp << ", Ct = " << Ct
+           << exit(FatalError);
+    }
+
+    // Calibrated thrust/power coeffs from power/thrust curves (LSRMTK:Eq. 6)
+    const scalar CtStar = Ct*sqr(magUref/magUdisk);
+    const scalar CpStar = Cp*pow3(magUref/magUdisk);
+
+    // Compute calibrated thrust/power (LSRMTK:Eq. 5)
+    const scalar T = 0.5*rhoRef*diskArea_*magSqr(Udisk & diskDir_)*CtStar;
+    const scalar P = 0.5*rhoRef*diskArea_*pow3(mag(Udisk & diskDir_))*CpStar;
+
+    for (const label celli : cells_)
+    {
+        Usource[celli] += (cellsV[celli]/totalV*T)*diskDir_;
+    }
+
+    if
+    (
+        mesh_.time().timeOutputValue() >= writeFileStart_
+     && mesh_.time().timeOutputValue() <= writeFileEnd_
+    )
+    {
+        Ostream& os = file();
+        writeCurrentTime(os);
+
+        os  << Uref << tab << Cp << tab << Ct << tab
+            << Udisk << tab << CpStar << tab << CtStar << tab << T << tab << P
+            << endl;
+    }
+}
+
+
+// ************************************************************************* //
