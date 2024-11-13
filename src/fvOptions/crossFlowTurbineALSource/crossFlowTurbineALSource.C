76566b199a22887db1c74ae3dadecf646f4995a1
Adhyanth <77678593+Adhyanth29@users.noreply.github.com>
2024-11-13 17:12:27 +0100

    Update crossFlowTurbineALSource.C
    Defaults the value of cone angle to 0, if the entry is not detected. This way, existing tutorials will not break.

--- src/fvOptions/crossFlowTurbineALSource/crossFlowTurbineALSource.C
+++ src/fvOptions/crossFlowTurbineALSource/crossFlowTurbineALSource.C
@@ -113,7 +113,9 @@ void Foam::fv::crossFlowTurbineALSource::createBlades()
             scalar chordLength = elementData[j][3];
             scalar chordMount = elementData[j][4];
             scalar pitch = elementData[j][5];
-            scalar cone = elementData[j][6];  // Reads in the cone angle (from rotation axis) in degrees
+            scalar cone = (elementData.size() > j && elementData[j].size() > 6)
+                                              ? elementData[j][6] : 0;
+            // Reads in the cone angle (from rotation axis) in degrees
 
             // Compute frontal area contribution from this geometry segment
             if (j > 0)
